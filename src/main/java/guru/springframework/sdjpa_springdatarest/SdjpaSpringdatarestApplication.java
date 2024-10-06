cbd4007fc603a2e949acf2eb73cc6d5421c3b925
Vutha Phai <vuthaphai@Vuthas-MacBook-Pro.local>
2024-10-06 11:48:30 +0700

    ïnitial project

---
+++ src/main/java/guru/springframework/sdjpa_springdatarest/SdjpaSpringdatarestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sdjpa_springdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaSpringdatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaSpringdatarestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sdjpa_springdatarest/SdjpaSpringdatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sdjpa_springdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaSpringdatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
